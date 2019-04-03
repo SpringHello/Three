37c52b27ce13a291f35e299018b4699971d9539c
sugar <804589369@qq.com>
2019-04-03 18:04:38 +0800

    git init

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+    dev: {},
+    build: {
+        index: path.join(__dirname, '../dist/index.html'),
+        assetsRoot: path.join(__dirname, '../dist'),
+        assetsSubDirectory: 'static'
+    }
+}
